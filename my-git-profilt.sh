3666bbe616cff4ff2ef79e15f18f77ef18e138e4
lufeng <837091544@qq.com>
2016-11-29 22:36:20 +0800

    Add files via upload

---
+++ my-git-profilt.sh
@@ -0,0 +1,7 @@
+git config --global user.name "lupiqian"
+git config --global user.email 837091544@qq.com
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global color.ui true
